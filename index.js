1dfffea229883a2c601474092c88d52a4d172b80
Nitin88362 <singhnitin5889@gmail.com>
2024-08-06 13:04:09 -0930

    First comment

---
+++ index.js
@@ -0,0 +1,8 @@
+// const scrollContainer = document.querySelectorAll('.product');
+// for(const item of scrollContainer){
+//     item.addEventListener('wheel',(evt)=>{
+//         evt.preventDefault();
+//         item.scrollLeft += evt.deltaY;
+        
+//     })
+// }
